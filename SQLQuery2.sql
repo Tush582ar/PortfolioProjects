8949a93168d94b2cd1919e611ba6d52e97b7ce01
Tush582ar <112242651+Tush582ar@users.noreply.github.com>
2022-08-27 13:54:18 +0530

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[Per_Country_Vaccinations_till_date]
-  FROM [Portfolio_Project].[dbo].[PercentPopulationVaccinated]
